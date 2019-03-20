27dbf7607cd6de2b62667724c730fbd5ad5ea1e0
Peter Krak <pkrak@csas.cz>
2019-03-20 10:10:27 +0100

    jquery
    .

--- API-manager/src/app/enviroments.ts
+++ API-manager/src/app/enviroments.ts
@@ -1,13 +1,13 @@
 import { Enviroment } from './enviroment';
 
 export const ENVIROMENTS: Enviroment[] = [
-      { id: 1, name: 'DEV A' },
-      { id: 2, name: 'DEV B' },
-      { id: 3, name: 'UAT 1' },
-      { id: 4, name: 'UAT 2' },
-      { id: 5, name: 'UAT 3' },
-      { id: 6, name: 'PRS' },
-      { id: 7, name: 'PRED' },
-      { id: 8, name: 'PROD' }
+      { id: 1, name: 'DEV A',url: 'url' },
+      { id: 2, name: 'DEV B',url: 'url' },
+      { id: 3, name: 'UAT 1',url: 'url' },
+      { id: 4, name: 'UAT 2',url: 'url' },
+      { id: 5, name: 'UAT 3',url: 'url' },
+      { id: 6, name: 'PRS',url: 'url' },
+      { id: 7, name: 'PRED',url: 'url' },
+      { id: 8, name: 'PROD',url:  'https://www.csas.cz/webapi/api' }
     ];
 
