f36e4416f774de47e4c9dc7181263bbf7969cab5
Peter Krak <pkrak@csas.cz>
2019-03-26 10:36:41 +0100

    .
    .

--- API-manager/src/app/app.component.ts
+++ API-manager/src/app/app.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
-import * as $ from 'jquery';
+import { Component, OnInit } from '@angular/core';
+import { ENVIROMENTS} from "./enviroments";
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'API-manager';
- // name = '';
+
+export class AppComponent implements OnInit{
+  pocetProstredi = ENVIROMENTS.length;
+  prostredi = ENVIROMENTS;
+  dropdown;
+  dropdowns = [] ;
+  hodnotaNaDropdown = 'Prostředí';
+  hodnotaEntity = 'Entita';
+  hodnotaCiloveProstredi = 'Prostředí';
+  hodnotaEntitaProstredi = 'Zvolte hodnotu';
+
+constructor(private http: HttpClient, config: NgbCarouselConfig) {
+    config.showNavigationIndicators = false;
+    config.showNavigationArrows = false;
+  }
+
+  ngOnInit(){
+    this.vypisProstredi();
+  }
+
+
+  vypisProstredi() {
+    let i;
+    for (i = 0; i < this.pocetProstredi; i++) {
+      this.dropdowns.push(this.prostredi[i].name);
+      // console.log(this.dropdowns);
+    }
+  }
+
+  ZmenaHodnoty(novaHodnotaNaDropdown: string) {
+    this.hodnotaNaDropdown = novaHodnotaNaDropdown;
+  }
+
+  ZmenaEntity(novaHodnotaEntity: string) {
+    this.hodnotaEntity = novaHodnotaEntity;
+  }
+
+  ZmenaCilovehoProstredi(novaHodnotaCilovehoProstredi: string) {
+    this.hodnotaCiloveProstredi = novaHodnotaCilovehoProstredi;
+  }
+
+
 }
+
